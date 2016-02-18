776097c3b83bf41e3bbf8563b1509d3b0dcc5c05
brld <brandon.lee.darby@gmail.com>
2016-02-18 12:40:13 -0800

    Added more user inputs, debuggin in process

--- index.php
+++ index.php
@@ -4,15 +4,24 @@
 
     <?php require "logic.php"; ?>
     <title>xkcd Password Generator</title>
-    <link rel="stylesheet" href="" type="text/css">
+    <link rel="stylesheet" href="main.css" type="text/css">
 
   </head>
   <body>
     <h1>Xkcd Password Generator</h1>
     <form action="logic.php" method="GET">
-      Number of words (Min 2, Max 9):
+      Number of words (Min 2, Max 9): <br>
       <input type="text" name="inputWords" maxlength="1" id="inputWords"> <br>
+      Include a number? <br>
+      <input type="checkbox" name="numberGen" value="Number" id="numberGen"> <br>
+      Include a symbol? <br>
+      <input type="checkbox" name="symbolGen" value="Symbol" id="symbolGen"> <br>
       <input type="submit" value="Generate new">
     </form>
+    <?php foreach ($words as $rand_words => $numWords) {
+      echo $words[$rand_words[$i]] . "\n";
+      if ($i < $numWords - 1) {
+        echo "- ";
+    } ?>
   </body>
 </html>
--- logicArchive.php
+++ logicArchive.php
@@ -1,13 +1,17 @@
 <?php
   $words = array("tasty", "pig", "pillow", "smart", "laptop", "couch", "red", "sharp", "pencil", "funny", "muffin", "paper", "elite", "bright", "sunny", "confused", "wet", "rough", "furry", "soft", "clean", "fast", "frisbee");
   $inputWords = $_GET["inputWords"];
-
+  
   if ($inputWords == 1) {
     $inputWords = 2;
     echo nl2br ("Invalid input, generating two word password. \n \n");
   }
   $numWords = $inputWords;
-  $rand_words = array_rand($words,$numWords);
-
+  $rand_keys = array_rand($words,$numWords);
+  for ($i=0; $i < $numWords; $i++) {
+    echo $words[$rand_keys[$i]] . "\n";
+    if ($i < $numWords - 1) {
+      echo "- ";
+    }
   }
 ?>
