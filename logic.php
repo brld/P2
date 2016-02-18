094fe25757c3af3f99911756fd4b1685f3ba26e4
brld <brandon.lee.darby@gmail.com>
2016-02-17 20:38:09 -0800

    Almost completely fixed array indexing error

--- logic.php
+++ logic.php
@@ -5,15 +5,18 @@
   $cycle = rand(1,$maxCycle);
   $passwordI = 0;
   $reindexedWords = array();
+  $iUnset;
   for ($j = 1; $j <= 4 /* = inputWords*/; $j++) {
     for ($i=0; $i <= $cycle; $i++) {
       if ($i == $cycle) {
         $password[$passwordI] = $words[$i];
-        unset($words[$i]);
-        $reindexedWords = array_values($array);
+        $iUnset = $i;
+        echo $words[$i];
       }
     }
     $passwordI++;
     $cycle = rand(1,$maxCycle - 1);
+    unset($words[$iUnset]);
+    $reindexedWords = array_values($words);
   }
 ?>
