62473c987e61100e8e4f29f6218cfc432c001854
brld <brandon.lee.darby@gmail.com>
2016-02-18 00:30:26 -0800

    Fixed input forms

--- index.php
+++ index.php
@@ -2,21 +2,17 @@
 <html>
   <head>
 
-    <?php require "logic.php" ?>
+    <?php require "logic.php"; ?>
     <title>xkcd Password Generator</title>
     <link rel="stylesheet" href="" type="text/css">
 
   </head>
   <body>
     <h1>Xkcd Password Generator</h1>
-    <?php $inputWords = 4; ?>
-    <form action="index.php" method="GET">
+    <form action="logic.php" method="GET">
       Number of words (Max 9):
       <input type="text" name="inputWords" maxlength="1" id="inputWords"> <br>
       <input type="submit" value="Generate new">
-      <?php
-      $inputWords= $_GET["inputWords"];
-      ?>
     </form>
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,11 +1,11 @@
 <?php
   $words = array("tasty", "pig", "pillow", "smart", "laptop", "couch", "red", "sharp", "pencil", "funny", "muffin", "paper", "elite", "bright", "sunny", "confused", "wet", "rough", "furry", "soft", "clean", "fast", "frisbee");
-
+  $inputWords = $_GET["inputWords"];
   $numWords = $inputWords;
   $rand_keys = array_rand($words,$numWords);
   for ($i=0; $i < $numWords; $i++) {
     echo $words[$rand_keys[$i]] . "\n";
-    if ($i != $numWords - 1) {
+    if ($i < $numWords - 1) {
       echo "- ";
     }
   }
